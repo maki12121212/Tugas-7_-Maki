c6369df2aef27215409fa9e0ed5eb397096941fe
maki12121212 <maki82913@gmail.com>
2023-06-21 10:36:16 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $nama = $_POST['nama'];
+    $role = $_POST['role'];
+    $availability = $_POST['availability'];
+    $age = $_POST['age'];
+    $lokasi = $_POST['lokasi'];
+    $experience = $_POST['experience'];
+    $email = $_POST['email'];
+
+    
+}
+?>
